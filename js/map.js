ef0a76ca0942e46651a8e22ba4a27df138bdc549
EricWehrly <eric@disruptiontheory.com>
2017-04-22 20:48:44 -0400

    click makes a marker (for now)

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 var L = require('leaflet');
 require('esri-leaflet');
-//var popupTemplate = require('.src/popup.js');
+// var popupTemplate = require('.src/popup.js');
+
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
 var map = L.map('map').setView([32.837, -83.632], 10);
 
 var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,7 +10,24 @@ var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+function onMapClick(e) {
+    
+    console.log(e);
+    /*
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    */
 
+    var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+}
+
+map.on('click', onMapClick);
+
+
+// TODO: Try / catch with instructions ...
 var SerialPort = require('serialport');
 var port = new SerialPort.SerialPort('/dev/ttyUSB0', { // change path
     baudrate: 4800,
